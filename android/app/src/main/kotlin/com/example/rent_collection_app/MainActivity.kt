034ce4961c0852e418b8eeeb8da1e1aa8ecce006
rajgowri <rajgowri090@gmail.com>
2024-04-10 12:12:55 +0530

    New Project

--- android/app/src/main/kotlin/com/example/rent_collection_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/rent_collection_app/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.rent_collection_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() { // No additional code needed here
+}
